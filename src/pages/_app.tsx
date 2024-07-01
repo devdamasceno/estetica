6393b1c5de58e338b5d351539d837b500838f423
Sergio Damasceno <sergiodamasceno@Mac-mini-de-Sergio.local>
2024-06-30 21:27:39 -0300

    page ok

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import '../styles/globals.css';
-import '@fortawesome/fontawesome-svg-core/styles.css'; 
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
-import '../../lib/fontAwesome'; 
+import { Analytics } from '@vercel/analytics/react';
+import '../../lib/fontAwesome';
 
 config.autoAddCss = false;
 
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp;
