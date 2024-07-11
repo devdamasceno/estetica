5e5149b39a40d8f2fc5a7745d5ecc92ef3659cc1
Sérgio Damasceno <sergio@suporteinfojf.com.br>
2024-07-11 15:12:29 +0000

    update core

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
+};
