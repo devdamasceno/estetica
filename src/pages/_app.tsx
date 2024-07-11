0871e067dc5f307761818fbc336ce0c88179cca7
Sérgio Damasceno <sergio@suporteinfojf.com.br>
2024-07-11 15:50:18 +0000

    update animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,19 +3,23 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import { Analytics } from '@vercel/analytics/react';
 import '../../lib/fontAwesome';
-import '../../public/styles/globals.css';
 
 config.autoAddCss = false;
 
 import type { AppProps } from 'next/app';
 
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
     <>
+      <Head>
+        <link rel="stylesheet" href="/styles/globals.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-1C8XN4XjqK8g93GG7QXz9zq5fCnLfvKpg7V9ca5FZB0Chz69UO3r22UdoJKKlj0a" crossOrigin="anonymous" />
+      </Head>
       <AnimatePresence mode="sync">
         <motion.div
           key={router.route}
