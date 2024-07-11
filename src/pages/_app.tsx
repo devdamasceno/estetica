d8efe1b2c922a6362c3a62227eb18d40a3165fd3
Sérgio Damasceno <sergio@suporteinfojf.com.br>
2024-07-11 16:04:52 +0000

    update animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,16 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { config } from '@fortawesome/fontawesome-svg-core';
 import { Analytics } from '@vercel/analytics/react';
-import '../../lib/fontAwesome';
-
-config.autoAddCss = false;
 
 import type { AppProps } from 'next/app';
 
 import { useRouter } from 'next/router';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
     <>
-      <Head>
-        <link rel="stylesheet" href="/styles/globals.css" />
-      </Head>
       <AnimatePresence mode="sync">
         <motion.div
           key={router.route}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="pt">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="stylesheet" href="/styles/globals.css" />
+        <link rel="icon" href="/favicon.svg" /> 
+      </Head>
       <body>
         <Main />
         <NextScript />
