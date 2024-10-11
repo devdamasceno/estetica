1be49c03bd0c479d96570efbb9b8d785c009d718
Sérgio Damasceno <sergio@suporteinfojf.com.br>
2024-10-11 19:51:54 +0000

    ajuste mobile

--- src/components/servicos/index.tsx
+++ src/components/servicos/index.tsx
@@ -1,9 +1,19 @@
 import styles from './styles.module.css';
 
 export function Servicos() {
-  return(
+  return (
     <div className={styles.Container}>
-      <h1>Servicos</h1>  
+      <div className={styles.Content}>
+        <div className={styles.ContentDescricao}>
+          <img src="/iconServico.webp" alt="Serviços" />
+          <h1>ESTÉTICA COM SAÚDE</h1>
+          <p>Nossa clínica conta com profissionais graduadas e capacitadas
+          para atendê-los de forma completa, cuidando do seu corpo de 
+          dentro pra fora.</p>
+        </div>
+        <div className={styles.ContentImagem}>
+        </div>
+      </div>
     </div>
   )
 }
